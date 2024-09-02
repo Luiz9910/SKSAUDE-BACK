408b4df1f6c7be0306d3eaa9b378998dd366cde8
Luiz Felipe <luiz.alves@sulwork.com.br>
2024-09-02 20:16:45 -0300

    DOCKER CONFIGURADO

---
+++ src/main/java/com/saude/sksaude/controller/PacienteController.java
@@ -0,0 +1,14 @@
+package com.saude.sksaude.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/paciente")
+public class PacienteController {
+    @GetMapping
+    public String getPaciente() {
+        return "putss";
+    }
+}
---
+++ src/test/java/com/saude/sksaude/SksaudeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saude.sksaude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SksaudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
