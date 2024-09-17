8335c4f3a29da891b0e63fc559dd3bf69456c363
Luiz Felipe <luizfelipealvesdesouza706@gmail.com>
2024-09-16 21:59:50 -0300

    swagger configurado e pastas criadas

--- src/main/java/com/saude/sksaude/controller/PacienteController.java
+++ src/main/java/com/saude/sksaude/controller/PacienteController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class PacienteController {
     @GetMapping
     public String getPaciente() {
-        return "putssss";
+        return "";
     }
 }
