bdeba97ee306fed99a387aa8be50b4be6ca566c9
Luiz Felipe <luiz.alves@sulwork.com.br>
2024-09-17 17:52:14 -0300

    classe global para controle das exceptions definda

--- src/main/java/com/saude/sksaude/repository/PatientRepository.java
+++ src/main/java/com/saude/sksaude/repository/PatientRepository.java
@@ -2,6 +2,7 @@ package com.saude.sksaude.repository;
 
 import com.saude.sksaude.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
