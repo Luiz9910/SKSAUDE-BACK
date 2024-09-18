68f7f096a9e7e0874dea1cc1302fa0ca211cfcc0
Marcelo Vinicios <marcelovinicios127@gmail.com>
2024-09-17 21:26:56 -0300

    adicionei meu nome e do meu marido

--- src/main/java/com/saude/sksaude/controller/PacienteController.java
+++ src/main/java/com/saude/sksaude/controller/PacienteController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class PacienteController {
     @GetMapping
     public String getPaciente() {
-        return "marcelo";
+        return "marcelo luiz";
     }
 }
